8d1644cbeb9f12022eaf88dca7cbe0d274392275
Marcos Gubitosi <marcosgubitosi@Marcoss-Air-2.fibertel.com.ar>
2023-05-24 01:21:32 -0300

    no toque el css ni el html, lo uncio cambie de lugar el html porque como es el main tiene que estar afuera, desps agregue un par de ftos pero para boludear, no cambie nada. desps cambie el nombre de un par de htmls y javas que si estan de acuerdo son los que voy a hacer que me fije en playground y tenian que tener esos nombres. desps nda, manana sigo

---
+++ Java/playlist.js
@@ -0,0 +1,4 @@
+// Es la página a donde el usuario podrá ver la lista de canciones que seleccionó  como favoritas.
+// Funcionalidad
+// La lista de temas debe recuperarse desde el storage del navegador y presentarse al usuario. Cada tema será un link que lleve a su correspondiente página de detalle. 
+// Entregable: Archivo playlist.html (o similar) y archivo playlist.js  (o similar)  junto con todos los recursos necesarios y  estilos de css.
