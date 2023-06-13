644332fc12ddbfdb9bad858fac4d2447381dbd22
bautistagm <bgarciamansilla@udesa.edu.ar>
2023-06-12 21:00:36 -0300

    no

--- Java/index.js
+++ Java/index.js
@@ -58,29 +58,3 @@ for (let i = 0; i < indice.length; i++) {
       indice[i].style.color = "black";
     });
 ;
-
-let fotosArtists = document.querySelectorAll(".imagenes")
-
-fotosArtists.forEach(fotosArtists=> {
-    fotosArtists.addEventListener("mouseover", function (){
-        fotosArtists.style.height = "135px"
-        fotosArtists.style.width = "120px"
-    })
-    fotosArtists.addEventListener("mouseout", function(){
-        fotosArtists.style.height = "100px"
-        fotosArtists.style.width = "100px"
-    })
-})
-
-let fotosAlbums = document.querySelectorAll(".ImagenesAlbum")
-
-fotosAlbums.forEach(fotosAlbums=>{
-    fotosAlbums.addEventListener("mouseover", function (){
-        fotosAlbums.style.height = "135px"
-        fotosAlbums.style.width = "135px"
-    })
-    fotosAlbums.addEventListener("mouseout", function(){
-        fotosAlbums.style.height = "100px"
-        fotosAlbums.style.width = "100px"
-    })
-})
